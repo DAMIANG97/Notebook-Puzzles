fba59534736492f05ead3020ca03cb5d857acbb5
DAMIANG97 <damiangrabowski975@gmail.com>
2024-03-18 21:20:46 +0100

    Change lv design, add hint mechanic
    - Change lv design,
    - Add hint mechanic
    - Add ship mechanic
    - Add new grapgics

---
+++ Assets/Hint.cs
@@ -0,0 +1,19 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Collections.Generic;
+
+public class Hint : MonoBehaviour
+{
+
+    public TextMeshProUGUI hintText;
+    private int currentIndex = 0;
+    public List<string> hints = new List<string>(); 
+
+    public void ChangeText()
+    {
+        hintText.text = hints[currentIndex];
+        currentIndex = (currentIndex + 1) % hints.Count;
+    }
+}
---
+++ Assets/Skip.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Skip : MonoBehaviour
+{
+        private int currentSceneIndex;
+public void SkipScene () {
+        currentSceneIndex= SceneManager.GetActiveScene().buildIndex;
+        Debug.Log(currentSceneIndex);
+        SceneManager.LoadScene(currentSceneIndex + 1);
+}
+}
