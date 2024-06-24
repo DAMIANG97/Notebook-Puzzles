c1bccae1a9124b8853e6210ae18185a0842b57d8
DAMIANG97 <damiangrabowski975@gmail.com>
2024-06-24 22:55:11 +0200

    Published version
    - Add rateup mechanics
    - minor design changes

---
+++ Assets/Scripts/RateUp.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RateUp : MonoBehaviour
+{
+
+    private string googlePlayURL = "https://play.google.com/store/apps/details?id=com.PixelPuzzles.NotebookPuzzles";
+
+    public void Rateup()
+    {
+#if UNITY_ANDROID
+            Application.OpenURL(googlePlayURL);
+#endif
+    }
+}
