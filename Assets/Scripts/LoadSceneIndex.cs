781c0821ae6d0beb638193e5aae24df0aef1ab0b
DAMIANG97 <damiangrabowski975@gmail.com>
2024-07-17 22:30:09 +0200

    Design new add mechanics

--- Assets/Scripts/LoadSceneIndex.cs
+++ Assets/Scripts/LoadSceneIndex.cs
@@ -11,11 +11,13 @@ public class LoadSceneIndex : MonoBehaviour
 
         int currentSceneIndex = GameManager.currentSceneIndex;
         SceneNumber = GameObject.Find("SceneNumber").GetComponent<TextMeshProUGUI>();
-        if(currentSceneIndex==0){
+        if (currentSceneIndex == 0)
+        {
             SceneNumber.text = "LEVEL 1";
         }
-        else{
-        SceneNumber.text = "LEVEL " + currentSceneIndex.ToString();
+        else
+        {
+            SceneNumber.text = "LEVEL " + currentSceneIndex.ToString();
         }
     }
 }
