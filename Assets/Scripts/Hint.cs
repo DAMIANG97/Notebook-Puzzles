bcde949593c1eba1c25ef9ee400eb97f264c21c3
DAMIANG97 <damiangrabowski975@gmail.com>
2024-08-12 00:03:54 +0200

    Bugfix

--- Assets/Scripts/Hint.cs
+++ Assets/Scripts/Hint.cs
@@ -10,7 +10,7 @@ public class Hint : MonoBehaviour
     public GameObject HintButton;
     public Button InfoButton;
     public TextMeshProUGUI InfoText;
-    private int currentIndex = 0;
+    private int currentIndex = 0; // Zaczynamy od 0
     public List<string> hints = new List<string>();
 
     private float lastChangeTime;
@@ -20,8 +20,11 @@ public class Hint : MonoBehaviour
     private void Start()
     {
         lastChangeTime = -minChangeInterval;
-        hintText.text = "";
         SkipButton.SetActive(false);
+
+
+        hintText.text = $"(1/{hints.Count}) hint: {hints[currentIndex]}";
+        currentIndex = currentIndex + 1;
     }
 
     public void ChangeText()
@@ -30,24 +33,29 @@ public class Hint : MonoBehaviour
         {
             if (!hintsCompleted)
             {
-                if (currentIndex >= hints.Count)
-                {
-                    hintsCompleted = true;
-                    hintText.text = "";
-                    SkipButton.SetActive(true);
-                    HintButton.SetActive(false);
-                }
-                else
+                // Sprawdzenie, czy są dostępne wskazówki
+                if (currentIndex < hints.Count)
                 {
                     int currentCoins = PlayerPrefs.GetInt("Coins", 100);
                     if (currentCoins >= 50)
                     {
                         PlayerPrefs.SetInt("Coins", currentCoins - 50);
                         PlayerPrefs.Save();
-                        hintText.text = $"({currentIndex + 1}/{hints.Count}) hint: {hints[currentIndex]}";
-                        currentIndex++;
-                        lastChangeTime = Time.time;
 
+                        // Wyświetlenie wskazówki
+                        hintText.text = $"({currentIndex + 1}/{hints.Count}) hint: {hints[currentIndex]}";
+                        currentIndex++; // Zwiększenie indexu po wyświetleniu
+
+                        // Sprawdzenie, czy to ostatnia wskazówka
+                        if (currentIndex > hints.Count)
+                        {
+                            hintsCompleted = true;
+                            SkipButton.SetActive(true);
+                            HintButton.SetActive(false);
+                            hintText.text = ""; // Opcjonalne, aby wyczyścić pole tekstowe
+                        }
+
+                        lastChangeTime = Time.time;
                     }
                     else
                     {
